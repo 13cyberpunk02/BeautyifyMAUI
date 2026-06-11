0313a8a4a978b2dfd21b9acbb3bd84ff959b26ea
13cyberpunk02 <salawat1302@gmail.com>
2026-06-11 09:21:05 +0300

    Добавьте файлы проекта.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BeautyifyMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
